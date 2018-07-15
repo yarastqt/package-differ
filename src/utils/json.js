5d96824c2f90992f4b583e4c5b3c4fd1e9d98c07
yarastqt <yarastqt@gmail.com>
2018-07-15 15:01:46 +0300

    refactor(utils/json): add generic type

--- src/utils/json.js
+++ src/utils/json.js
@@ -9,7 +9,7 @@ import { readFileSync } from 'fs'
  * @param filePath path to json file
  * @return object
  */
-export function readJson(filePath: string) {
+export function readJson<T>(filePath: string): T {
   const content = readFileSync(filePath, 'utf-8')
   const data = JSON.parse(content)
 
